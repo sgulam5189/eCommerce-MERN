973c1e3be4b5299a654d9462510b1f68f4589085
sgulam5189 <samdanicse07@gmail.com>
2022-11-19 17:29:56 -0500

    product Screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,29 @@
 import React from 'react'
+import {LinkContainer} from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return     <header>
     <Navbar bg="dark"  variant='dark' collapseOnSelect expand="lg">
       <Container>
-        <Navbar.Brand href="#home">ProShop</Navbar.Brand>
+        <LinkContainer to='/'>
+        <Navbar.Brand >ProShop</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-            <Nav.Link href="/login"><i className='fas fa-user'></i>Sign in</Nav.Link>
-            
+          <Nav className="ml-auto">
+            <LinkContainer to='/cart'>
+               <NavLink>
+                    <i className='fas fa-shopping-cart'></i>Cart 
+                </NavLink> 
+            </LinkContainer>
+            <LinkContainer to='/login'>
+                <NavLink>
+                    <i className='fas fa-user'></i>Sign in
+                </NavLink>
+            </LinkContainer>
+                       
           </Nav>
         </Navbar.Collapse>
       </Container>
